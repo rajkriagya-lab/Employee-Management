02cf1761518553c87bf7cedb7ddf2824794cfa6a
rajkriagya-lab <rajkriagya@gmail.com>
2026-09-06 22:51:44 +0545

    Merge branch 'main' of https://github.com/rajkriagya-lab/Business-Management

---
+++ frontend/src/components/auth/authLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function authLayout({title, subitle, children}) {
+  return (
+    <div className='min-h-screen '>
+      
+    </div>
+  )
+}
--- frontend/src/components/auth/button.jsx
+++ frontend/src/components/auth/button.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function button() {
+export default function button({ children, ...props }) {
   return (
-    <div>
-      
-    </div>
+    <button
+      {...props}
+      className="w-full bg-btn text-white py-3 rounded-lg cursor-pointer hover:opacity-90"
+    >
+      {children}
+    </button>
   )
 }
